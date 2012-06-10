5f908333cda533a1947f3031c6265fe125a35e78
Ido Green <greenido@gmail.com>
2012-06-10 12:03:17 +0300

    simple db changes
    simple db changes... more to come.

--- cellar.sql
+++ cellar.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.5.15, for osx10.6 (i386)
 --
--- Host: localhost    Database: cellar
+-- Host: localhost    Database: bira
 -- ------------------------------------------------------
 -- Server version	5.5.15
 
